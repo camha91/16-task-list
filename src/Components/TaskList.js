fb4ee1fd7dd0a1f2875bbe53c921bcd0b29bc704
camha91 <crystalhanguyen@gmail.com>
2021-04-16 15:09:04 -0700

    Create UI layout for TaskList

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -4,12 +4,86 @@ import { ThemeProvider } from "styled-components";
 import { TaskListDarkTheme } from "../Themes/TaskListDarkTheme";
 import { TaskListLightTheme } from "../Themes/TaskListLightTheme";
 import { TaskListDefaultTheme } from "../Themes/TaskListDefaultTheme";
+import { Dropdown } from "./Dropdown.js";
+import { Heading1, Heading2, Heading3, Heading4, Heading5 } from "./Heading.js";
+import { TextField } from "./TextField.js";
+import { Button } from "./Button.js";
+import { Table, Thead, Tbody, Tr, Td, Th } from "./Table.js";
 
 class TaskList extends Component {
   render() {
     return (
-      <ThemeProvider theme={TaskListLightTheme}>
-        <Container>Task List</Container>
+      <ThemeProvider theme={TaskListDarkTheme}>
+        <Container className="w-50">
+          <Dropdown>
+            <option>Dark Theme</option>
+            <option>Light Theme</option>
+            <option>Default Theme</option>
+          </Dropdown>
+          <Heading3>Task To Do</Heading3>
+          <TextField label="Task name" className="w-50" />
+          <Button className="ml-2">
+            <i className="fa fa-plus">Add Task</i>
+          </Button>
+          <Button className="ml-2">
+            <i className="fa fa-upload">Update Task</i>
+          </Button>
+          <br />
+          <Table>
+            <Thead>
+              <Tr>
+                <Th style={{ verticalAlign: "middle" }}>Task name</Th>
+                <Th className="text-right">
+                  <Button className="ml-2">
+                    <i className="fa fa-edit"></i>
+                  </Button>
+                  <Button className="ml-2">
+                    <i className="fa fa-check"></i>
+                  </Button>
+                  <Button className="ml-2">
+                    <i className="fa fa-trash"></i>
+                  </Button>
+                </Th>
+              </Tr>
+              <Tr>
+                <Th style={{ verticalAlign: "middle" }}>Task name</Th>
+                <Th className="text-right">
+                  <Button className="ml-2">
+                    <i className="fa fa-edit"></i>
+                  </Button>
+                  <Button className="ml-2">
+                    <i className="fa fa-check"></i>
+                  </Button>
+                  <Button className="ml-2">
+                    <i className="fa fa-trash"></i>
+                  </Button>
+                </Th>
+              </Tr>
+            </Thead>
+          </Table>
+
+          <Heading3>Task Completed</Heading3>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th style={{ verticalAlign: "middle" }}>Task name</Th>
+                <Th className="text-right">
+                  <Button className="ml-2">
+                    <i className="fa fa-trash"></i>
+                  </Button>
+                </Th>
+              </Tr>
+              <Tr>
+                <Th style={{ verticalAlign: "middle" }}>Task name</Th>
+                <Th className="text-right">
+                  <Button className="ml-2">
+                    <i className="fa fa-trash"></i>
+                  </Button>
+                </Th>
+              </Tr>
+            </Thead>
+          </Table>
+        </Container>
       </ThemeProvider>
     );
   }
